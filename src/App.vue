f821ab962edb1073052fbd76a49fc2e7f601a47b
danish <danishmehmood981@gmail.com>
2024-04-23 18:35:41 +0500

    watching videos along to build todo application

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import TodoHeaver from './components/TodoHeaver.vue';
 
 </script>
 
 <template>
+  <TodoHeaver />
   <RouterView />
 </template>
 
 
-<style>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap');
+
+
+* {
+  font-family: "Rubik, sans-serif";
+  margin : 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 1100px;
+  margin: 0 auto;
+}
+
+
+
 </style>
