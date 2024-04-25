4532037663c6aca3e28c03b4ed999e1508c57447
danish <danishmehmood981@gmail.com>
2024-04-25 17:56:25 +0500

    making the input reactive with ref and reactive methods imported vue from vue

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,13 @@ import TodoHeader from "./components/TodoHeader.vue";
   <RouterView />
 </template>
 
-
 <style lang="scss">
+
 @import url("https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap");
 
+/* if you define the fallback font, it will be used if the browser doesn't support the font, but in this case it is not working, if we define sans-serif , it wont be working */
 * {
-  font-family: "Rubik, sans-serif";
+  font-family: "Rubik";
   margin: 0;
   padding: 0;
   box-sizing: border-box;
---
+++ src/views/TodosView.vue
@@ -0,0 +1,28 @@
+<script setup>
+import TodoCreater from "@/components/TodoCreater.vue";
+
+
+</script>
+
+<template>
+  <main>
+    <h1>Todos application</h1>
+    <TodoCreater />
+  </main>
+</template>
+
+<style lang="scss" scoped>
+  main {
+    display: flex;
+    flex-direction: column;
+    max-width: 500px;
+    width : 100%;
+    margin : 0 auto;
+    padding : 40px 16px;
+    h1 {
+      margin-bottom: 16px;
+      text-align: center;
+    }
+  }
+</style>
+
